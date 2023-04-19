128a1ea7deba22989cb62e358113fe22832236d8
jersoncesar20 <131275174+jersoncesar20@users.noreply.github.com>
2023-04-19 09:44:37 -0500

    subir primera plantilla

---
+++ footer.php
@@ -0,0 +1,51 @@
+<footer class="bg-dark text-white text-center text-lg-start">
+  <div class="container p-4">
+    <div class="row">
+      <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+        <h5 class="text-uppercase">Contenido del Footer</h5>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
+          molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae
+          aliquam voluptatem veniam, est atque cumque eum delectus sint!
+        </p>
+      </div>
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <h5 class="text-uppercase">Links</h5>
+        <ul class="list-unstyled mb-0">
+          <li>
+            <a href="#!" class="text-white">Facebook</a>
+          </li>
+          <li>
+            <a href="#!" class="text-white">WhatsApp</a>
+          </li>
+          <li>
+            <a href="#!" class="text-white">Tiktok</a>
+          </li>
+          <li>
+            <a href="#!" class="text-white">Instagram</a>
+          </li>
+        </ul>
+      </div>
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <h5 class="text-uppercase">Links</h5>
+        <ul class="list-unstyled mb-0">
+          <li>
+            <a href="#!" class="text-white">Facebook</a>
+          </li>
+          <li>
+            <a href="#!" class="text-white">WhatsApp</a>
+          </li>
+          <li>
+            <a href="#!" class="text-white">Tiktok</a>
+          </li>
+          <li>
+            <a href="#!" class="text-white">Instagram</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2023 Desarrolado por Jerson Quispe Perez. Todos los derechos reservados.
+  </div>
+</footer>
---
+++ index.php
@@ -0,0 +1,126 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Mi Pagina Web</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+   
+  </head>
+  
+<body >
+<style>
+    body{
+    
+    background:radial-gradient(circle, black, #1162ac);
+}
+h1{
+    color: white;
+}
+</style>
+
+
+<?php include('menu.php'); ?>
+
+
+<div class="container my=3">
+    
+     <h1 class="text-center">Trabajo Diseño de Soluciones Web</h1> <br>
+    
+     <div class="row">
+
+       <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         <div class="card">
+           <img src="img/crist.jpg" alt="Imagen 1" class="card-img-top">
+           <div class="card-body">
+             <h5 class="card-title">Jugadores</h5>
+             <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, fuga? Similique, nulla porro eos architecto consequatur eaque beatae assumenda voluptatum repudiandae doloribus blanditiis facere quo aspernatur cum, eligendi quaerat eius!</p>
+             <a href="#" class="btn btn-primary">Comprar</a>
+             <a href="#" class="btn btn-primary">Añadir al carrito</a>
+           </div>
+         </div>
+       </div>
+      
+       <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         <div class="card">
+           <img src="img/halan.jpg" alt="Imagen 2" class="card-img-top">
+           <div class="card-body">
+             <h5 class="card-title">Jugadores</h5>
+             <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta nam adipisci sit praesentium sunt obcaecati eos culpa quidem inventore facilis, animi esse deserunt itaque sequi aperiam sed, ab quia dolores!</p>
+             <a href="#" class="btn btn-primary">Comprar</a>
+             <a href="#" class="btn btn-primary">Añadir al carrito</a>
+           </div>
+         </div>
+       </div>
+      
+      
+      
+       <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         <div class="card">
+           <img src="img/jersfr.jpg" alt="Imagen 3" class="card-img-top">
+           <div class="card-body">
+             <h5 class="card-title">Jugadores</h5>
+             <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem recusandae error est tenetur dolores, pariatur quibusdam perspiciatis quaerat! Quisquam ad accusamus corrupti. Natus, numquam. Magni exercitationem quasi ullam repudiandae architecto.</p>
+             <a href="#" class="btn btn-primary">Comprar</a>
+             <a href="#" class="btn btn-primary">Añadir al carrito</a>
+           </div>
+         </div>
+        </div>
+        
+       
+     
+     
+    
+      <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         <div class="card">
+           <img src="img/mbappe.jpg" alt="Imagen 4" class="card-img-top">
+           <div class="card-body">
+             <h5 class="card-title">Jugadores</h5>
+             <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque atque ratione totam molestias beatae sequi ullam nostrum quisquam cupiditate ex alias, mollitia magni animi sunt! Quibusdam, assumenda illum. Aspernatur, assumenda.</p>
+             <a href="#" class="btn btn-primary">Comprar</a>
+             <a href="#" class="btn btn-primary">Añadir al carrito</a>
+           </div>
+         </div>
+       </div>
+      
+       
+      
+       <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         <div class="card">
+           <img src="img/Neymar.jpg" alt="Imagen 5" class="card-img-top">
+           <div class="card-body">
+             <h5 class="card-title">Jugadores</h5>
+             <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, accusamus inventore. Magni error exercitationem, culpa sunt harum recusandae ipsa fugit nulla ipsam esse necessitatibus tenetur atque ut quibusdam quasi reiciendis.</p>
+             <a href="#" class="btn btn-primary">Comprar</a>
+             <a href="#" class="btn btn-primary">Añadir al carrito</a>
+           </div>
+         </div>
+       </div>
+       
+       <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         <div class="card">
+           <img src="img/vinicios.jpg" alt="Imagen 6" class="card-img-top">
+           <div class="card-body">
+             <h5 class="card-title">Jugadores</h5>
+             <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque blanditiis officiis explicabo reprehenderit dolores nostrum est inventore id unde molestiae dolor, quibusdam ipsum ea modi dolorem repudiandae necessitatibus itaque tenetur.</p>
+             <a href="#" class="btn btn-primary">Comprar</a>
+             <a href="#" class="btn btn-primary">Añadir al carrito</a>
+           </div>
+         </div>
+       </div>
+       </div> 
+       </div><br>
+      
+
+       <?php include('footer.php'); ?>
+ 
+       
+  
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+
+
+
+
+</body>
+
